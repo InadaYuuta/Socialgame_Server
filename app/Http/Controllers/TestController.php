954fa26a0d53979970cd37f937ad57b5a29367e8
InadaYuuta <yuunoa00@gmail.com>
2024-09-13 14:13:23 +0900

    [#7915]追加実装・調整
    crontestようにコード変更

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -5,8 +5,6 @@ namespace App\Http\Controllers;
 class TestController extends Controller
 {
     public function __invoke(){
-        $file = "/home/inada/socialgame_server/app/Cron/test.txt";
-        $text = "こんにちは"."\n";
-        return file_put_contents($file,$text,FILE_APPEND); // 一旦テストで書き込み
+        return "呼び出されました。";
     }
 }
