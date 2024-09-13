42843045329add3a8ab65ab34664130029a80987
InadaYuuta <yuunoa00@gmail.com>
2024-09-13 14:00:14 +0900

    [#7915]追加実装・調整
    cronTestを行うためにコードを追記

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 class TestController extends Controller
 {
     public function __invoke(){
-        return 0;
+        $file = "/home/inada/socialgame_server/app/Cron/test.txt";
+        $text = "こんにちは"."\n";
+        return file_put_contents($file,$text,FILE_APPEND); // 一旦テストで書き込み
     }
 }
